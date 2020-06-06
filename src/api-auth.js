a181e8d24ce8d2610f3542e875f21eafbe6d8cdf
ibrahimshamma99 <i.abushammah@gmail.com>
2020-06-06 12:56:57 +0300

    📦 NEW:

--- src/api-auth.js
+++
@@ -1,9 +0,0 @@
-const signout = () => {
-  return fetch(url + "/logout", {
-    method: "GET",
-  })
-    .then((response) => {
-      return response.json();
-    })
-    .catch((err) => console.log(err));
-};
